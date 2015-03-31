d97ab9ace75ba6dfd6c100d4355942c8cacc0c58
slifty <slifty@gmail.com>
2015-03-31 14:52:11 -0400

    Initial application to load the UIview.

---
+++ Tally/Controllers/ErrorViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tally
+//
+//  Created by Daniel Schultz on 3/22/15.
+//  Copyright (c) 2015 Gender Avenger. All rights reserved.
+//
+
+import UIKit
+
+class ErrorViewController: UIViewController {
+    
+    @IBAction func tryAgainButtonHandler() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }    
+}
+
