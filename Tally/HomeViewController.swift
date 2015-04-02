ce27a1f0629308e7eba2c47b0bcb8847bc9f5853
slifty <slifty@gmail.com>
2015-04-02 10:50:22 -0400

    Added nicer styling and a loading screen.

--- Tally/HomeViewController.swift
+++ Tally/HomeViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 class HomeViewController: UIViewController, UIWebViewDelegate {
-
+    
     @IBOutlet var mainWebview: UIWebView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +20,12 @@ class HomeViewController: UIViewController, UIWebViewDelegate {
     override func viewDidAppear(animated:Bool) {
         super.viewDidAppear(animated)
         
+        // Hide the loading web view to prevent a flash of loading content
+        activityIndicator.startAnimating()
+        mainWebview.hidden = true;
+        
         // Attempt to load GA Tally
-        var urlString:NSString = "http://127.0.0.1:3000"
+        var urlString:NSString = "http://even-steven-yoni-test.herokuapp.com"
         var urlObject:NSURL = NSURL(string: urlString)!
         var urlRequest:NSURLRequest = NSURLRequest(URL: urlObject)
         mainWebview.delegate = self
@@ -48,10 +53,13 @@ class HomeViewController: UIViewController, UIWebViewDelegate {
         return true
     }
     
-    func webViewDidStartLoad(webView: UIWebView!) {
-    }
+    func webViewDidStartLoad(webView: UIWebView!) {    }
     
     func webViewDidFinishLoad(webView: UIWebView!) {
+        // Show the webview and stop the animation
+        mainWebview.hidden = false;
+        activityIndicator.stopAnimating()
+        
         // This is a hack to allow us to detect when a link would open a new tab
         var JSInjection: NSString = "javascript: var allLinks = document.getElementsByTagName('a'); if (allLinks) {var i;for (i=0; i<allLinks.length; i++) {var link = allLinks[i];var target = link.getAttribute('target'); if (target && target == '_blank') {link.setAttribute('target','_self');link.href = 'newtab:'+link.href;}}}";
         webView.stringByEvaluatingJavaScriptFromString(JSInjection);
